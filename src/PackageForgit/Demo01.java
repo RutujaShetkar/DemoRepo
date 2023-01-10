c6d55829cb8daa294c51d794886fbbd699eccb2c
Rutuja Shetkar <rutujashetkar0726@gmail.com>
2023-01-10 16:51:57 +0530

    added

---
+++ src/PackageForgit/Demo01.java
@@ -0,0 +1,25 @@
+package PackageForgit;
+
+import java.util.List;
+
+
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		
+			
+		
+		int r,n=4,c;
+		for(r=0;r<n;r++)
+		{
+			for(c=n;c>r;c--)
+				System.out.print("*");
+			System.out.print("\n");
+		}
+		  }
+		// TODO Auto-generated method stub
+
+	}
+
+
